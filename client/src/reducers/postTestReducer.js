951712098e1791ca8712c16a508c197a02a88b84
byungminkim <byungminkim12@gmail.com>
2021-08-10 08:21:44 +0900

    ✅Add:userLogInstatus/Refactor: unused data/porps

--- client/src/reducers/postTestReducer.js
+++ client/src/reducers/postTestReducer.js
@@ -2,17 +2,17 @@ import { initialState } from './initialState';
 import { ALL_POST_TEST } from '../actions/postActionIndex';
 
 const postTestReducer = (state = initialState.data, action) => {
-  const newState = [...state];
+  console.log(state);
+  const newState = [...state.posts];
   console.log(newState);
   switch (action.type) {
     case ALL_POST_TEST:
-      // return Object.assign({}, state, {
-      //   items: action.payload.items
-      // });
+    // return Object.assign({}, state, {
+    //   items: action.payload.items
+    // });
     default:
       return state;
   }
-
   return newState;
 };
 
