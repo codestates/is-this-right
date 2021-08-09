0c558032e58de1e158c983c6de69246b6a8577d6
HunKimADev <sanghunkim.nz@gmail.com>
2021-08-10 03:23:03 +0900

    ✅Add: posts API - getAll, getDetail, put, delete, post / multer setting and connecting sto s3 to upload filles

--- server/router.js
+++ server/router.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('./controllers');
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const s3Config = require(__dirname + '/config/s3');
+const s3 = new aws.S3(s3Config);
+const uuid = require('uuid');
+require('dotenv').config();
+
+let upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      cb(null, `uploads/${Date.now()}_${uuid.v4()}`);
+    },
+  }),
+});
 
 //users routing
 router.get('/users', controllers.users.get);
@@ -22,16 +41,16 @@ router.post('/feedbacks', controllers.feedbacks.post);
 router.delete('/feedbacks/:id', controllers.feedbacks.delete);
 
 //posts routing
-router.get('/posts', controllers.posts.get);
-router.get('/posts/:id', controllers.posts.get);
-router.put('/posts/:id', controllers.posts.put);
-router.post('/posts', controllers.posts.post);
+router.get('/posts', controllers.posts.getAll);
+router.get('/posts/:id', controllers.posts.getDetail);
+router.put('/posts/:id', upload.array('files'), controllers.posts.put);
+router.post('/posts', upload.array('files'), controllers.posts.post);
 router.delete('/posts/:id', controllers.posts.delete);
 
 //chats routing
 router.get('/chats', controllers.chats.get);
 router.post('/chats', controllers.chats.post);
-router.get('/chats/messages', controllers.messages.get);
+router.get('/chats/messages/:id', controllers.messages.get);
 router.post('/chats/messages', controllers.messages.post);
 
 router.get('/', (req, res) => {
