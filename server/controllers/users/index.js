028e5851bc8443834acb5958b24ca46415cf7434
shpk93 <shpk930924@gmail.com>
2021-08-10 09:14:57 +0900

    ✅Add: user api

--- server/controllers/users/index.js
+++ server/controllers/users/index.js
@@ -1,4 +1,64 @@
+const { user } = require('../../models');
+const { isAuthorized, generateAccessToken, sendAccessToken } = require('../tokenFunctions');
+
 module.exports = {
-  get: (req, res) => {},
-  put: (req, res) => {},
+  get: async (req, res) => {
+    if (req.query.email || req.query.username) {
+      //파라미터가 있을때(회원가입 중복체크 용도)
+      let findKey, findname;
+      if (req.query.email) {
+        findkey = 'email';
+        findname = req.query.email;
+      }
+      if (req.query.username) {
+        findkey = 'username';
+        findname = req.query.username;
+      }
+      let userdata = await user.findOne({ where: { [findkey]: findname } }).catch((err) => {
+        console.log(err);
+        return res.status(500).json({ message: 'database err' });
+      });
+
+      if (!userdata) {
+        return res.status(200).json({ message: 'ok' });
+      }
+      res.status(409).json({ message: 'this parameter is already use' });
+    } else {
+      //파라미터가 없을때 (유저정보 보내주기)
+      let userInfo = isAuthorized(req);
+      if (userInfo) {
+        res.json({ data: userInfo, message: 'ok' });
+      } else {
+        res.cookie('jwt', '', { httpOnly: true, maxAge: 1 });
+        res.status(401).json({ message: 'token has expired try again login' });
+      }
+    }
+  },
+
+  put: async (req, res) => {
+    let userInfo = isAuthorized(req);
+    if (userInfo) {
+      let { username, password, profileImg } = req.body;
+      let payload = {};
+      if (username) payload.username = username;
+      if (password) payload.password = password;
+      if (profileImg) payload.profileImg = profileImg;
+      let id = userInfo.id;
+
+      let updateUserInfo = await user.update(
+        { ...payload },
+        {
+          where: {
+            id,
+          },
+        },
+      );
+      delete userInfo.iat, userInfo.password;
+      userInfo = { ...userInfo, ...payload };
+      let token = generateAccessToken(userInfo);
+      sendAccessToken(res, token);
+    } else {
+      res.status(401).json({ message: 'Unauthorized request' });
+    }
+  },
 };
--- server/controllers/users/signin.js
+++ server/controllers/users/signin.js
@@ -1,3 +1,39 @@
-module.exports = (req, res) => {
-  res.send();
+const { user, adviser } = require('../../models');
+const { generateAccessToken, sendAccessToken } = require('../tokenFunctions');
+
+module.exports = async (req, res) => {
+  let { email, password } = req.body;
+
+  let userInfo = await user
+    .findOne({
+      where: {
+        email,
+        password,
+      },
+      include: [{ model: adviser, required: false }],
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ message: 'database err' });
+    });
+
+  if (userInfo) {
+    // adviser일때
+    if (userInfo.adviser) {
+      let adviserInfo = userInfo.dataValues.adviser;
+      delete userInfo.dataValues.adviser;
+      userInfo = { ...userInfo.dataValues, ...adviserInfo.dataValues };
+      userInfo.role = 'adviser';
+    } else {
+      // user일때
+      userInfo = userInfo.dataValues;
+      userInfo.role = 'user';
+      delete userInfo.adviser;
+    }
+    delete userInfo.password;
+    const accessToken = generateAccessToken(userInfo);
+    sendAccessToken(res, accessToken);
+  } else {
+    res.status(404).json({ message: { message: 'invalid user' } });
+  }
 };
