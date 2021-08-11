1d9dba83ed05accad6ed6920b2a8473e2dc18c79
HunKimADev <sanghunkim.nz@gmail.com>
2021-08-11 18:26:40 +0900

    ✅Add: Automated deployment aws-cli install scritps

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -7,3 +7,6 @@ sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --updates
