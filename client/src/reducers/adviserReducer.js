41a38dc38911d65043d6b6b60baac61a2b4e1f1f
byungminkim <byungminkim12@gmail.com>
2021-08-12 03:51:04 +0900

    ✅Add: add adviserReducer

---
+++ client/src/reducers/adviserReducer.js
@@ -0,0 +1,27 @@
+import { initialState } from './initialState';
+import { HANDLE_ADVISERPROFILE_IMG } from '../actions/adviserActionIndex';
+
+const adviserReducer = (state = initialState, action) => {
+  const newState = { ...state };
+  // console.log(newState);
+
+  if (action.type === HANDLE_ADVISERPROFILE_IMG) {
+    console.log('리듀서작동', action.payload.originFile);
+    let boolean = true;
+
+    newState.adviserProfileImg.imgFile.forEach((el) => {
+      if (el.uid === action.payload.originFile.uid) {
+        return (boolean = false);
+      }
+    });
+    if (boolean) {
+      newState.adviserProfileImg.imgFile = [...newState.adviserProfileImg.imgFile, action.payload.originFile];
+      newState.adviserProfileImg.preview = action.payload.preview;
+    }
+    return newState;
+  } else {
+    return state;
+  }
+};
+
+export default adviserReducer;
