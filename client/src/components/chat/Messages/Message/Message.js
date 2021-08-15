49a2c2440108d4224be0303aba2727000fc72552
shpk93 <shpk930924@gmail.com>
2021-08-15 21:51:12 +0900

    test2

--- client/src/components/chat/Messages/Message/Message.js
+++ client/src/components/chat/Messages/Message/Message.js
@@ -4,12 +4,12 @@ import './Message.css';
 
 import ReactEmoji from 'react-emoji';
 
-const Message = ({ message: { text, user }, name }) => {
+const Message = ({ message, name }) => {
   let isSentByCurrentUser = false;
 
   const trimmedName = name.trim().toLowerCase();
 
-  if (user === trimmedName) {
+  if (message.username === trimmedName) {
     isSentByCurrentUser = true;
   }
 
@@ -18,15 +18,15 @@ const Message = ({ message: { text, user }, name }) => {
     <div className="messageContainer justifyEnd">
       <p className="sentText pr-10">{trimmedName}</p>
       <div className="messageBox backgroundBlue">
-        <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+        <p className="messageText colorWhite">{ReactEmoji.emojify(message.message)}</p>
       </div>
     </div>
   ) : (
     <div className="messageContainer justifyStart">
       <div className="messageBox backgroundLight">
-        <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+        <p className="messageText colorDark">{ReactEmoji.emojify(message.message)}</p>
       </div>
-      <p className="sentText pl-10 ">{user}</p>
+      <p className="sentText pl-10 ">{message.username}</p>
     </div>
   );
 };
