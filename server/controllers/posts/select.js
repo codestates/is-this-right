eea0de435063bfa7fa3cc54fccc12e66dee5f7ab
HunKimADev <sanghunkim.nz@gmail.com>
2021-08-18 15:29:08 +0900

    :bug:Fix: select delete API

--- server/controllers/posts/select.js
+++ server/controllers/posts/select.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   delete: async (req, res) => {
     const userInfo = isAuthorized(req);
-    const { postId } = req.body;
+    const postId = req.params.id;
     const postInfo = await post.findOne({ where: { id: postId }, attributes: ['id', 'selected', 'userId'] });
     if (userInfo && userInfo.id === postInfo.dataValues.userId) {
       await sequelize.query(`UPDATE posts SET selected=null WHERE id=${postId}`, { type: QueryTypes.UPDATE });
