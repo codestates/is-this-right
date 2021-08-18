f681e335d0688418e66b3326aa1094d662b1481b
byungminkim <byungminkim12@gmail.com>
2021-08-18 15:26:35 +0900

    🐛Fix: fix upload router

--- server/controllers/uploads/index.js
+++ server/controllers/uploads/index.js
@@ -1,9 +1,13 @@
 module.exports = (req, res) => {
+  console.log('여기입니다.------------------', req.file);
   //body title, category, content, userId
   //files postId, url
-  const responseData = req.files.map((file) => {
-    return `![](${file.location})`;
-  });
+  // const responseData = req.files.map((file) => {
+  //   return `![](${file.location})`;
+  // });
+  const responseData = req.file.location;
 
-  res.status(201).json({ data: responseData, message: 'created' });
+  // res.status(201).json({ data: responseData, message: 'created' });
+  res.status(200).json({ uploaded: true, url: responseData });
+  // res.status(500).json({ uploaded: false, error: error });
 };
