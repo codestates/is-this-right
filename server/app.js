e7bf12eb9ec5dd178bec7ed0d478810dfd5faf30
HunKimADev <sanghunkim.nz@gmail.com>
2021-08-20 03:16:27 +0900

    :bug:Fix: deployment scripts

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
     origin: ['http://localhost:3000', 'http://localhost:3001', 'https://isthisright.ml'],
+    credentials: true,
   },
 });
 
