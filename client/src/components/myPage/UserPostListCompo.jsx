d8fa1f3c2cdd8334e122766a5ee98386b2133f11
Hoon <42455611+HunKimADev@users.noreply.github.com>
2021-08-25 10:11:43 +0900

    fix: mypage bug

--- client/src/components/myPage/UserPostListCompo.jsx
+++ client/src/components/myPage/UserPostListCompo.jsx
@@ -120,7 +120,7 @@ function UserPostListCompo() {
   return (
     <UserPostListCompoStyle>
       <SectionStyle role={userInfo.role}>
-        <h2 style={{ textAlign: 'center' }}>내가 올린 게시물</h2>
+        <div style={{ textAlign: 'center' }}>내가 올린 게시물</div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <MypostSectionStyle role={userInfo.role}>
             {currentPostPageList.map((el) => (
@@ -142,7 +142,7 @@ function UserPostListCompo() {
       <Divider />
       {userInfo.role === 'adviser' ? (
         <SectionStyle>
-          <h2 style={{ textAlign: 'center' }}>내가 답변한 게시물</h2>
+          <div style={{ textAlign: 'center' }}>내가 답변한 게시물</div>
           <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
             <MypostSectionStyle>
               {currentFeedbackPageList.map((el) => (
